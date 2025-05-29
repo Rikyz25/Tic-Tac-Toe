ce7199f6e1f71d1f39b7e6435aa8ebdae71692a8
Rikyz25 <anikbandopadhyay@gmail.com>
2025-05-29 12:54:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ let boxes=document.querySelectorAll(".box");
 let reset=document.querySelector("#reset");
 let winner=document.getElementById("winner");
 let turn0=false;
+let x=document.querySelector(".xwin");
+let o=document.querySelector(".owin");
+let xwin=0;
+let owin=0;
 let f=0;
 const winningPattern=[[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]];
 
@@ -32,6 +36,15 @@ const checkwinner=() =>{
             if(pos1val===pos2val && pos2val===pos3val){
                 console.log("Winner");
                 winner.innerText= "Congratualations!"+ pos1val + " has won";
+                if(pos1val=="X"){
+                    xwin++;
+                    x.innerText=xwin;
+                }
+                if(pos1val=="O"){
+                    owin++;
+                    o.innerText=owin;
+                     
+                } 
                 f=1;
                 boxes.forEach((box) =>{
                     box.disabled=true;
